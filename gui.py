7851eb3ccc032441bf580722e030dba888caadf5
Achilles Dabrowski <achilles.dabrowski@gmail.com>
2020-06-27 23:47:42 -0400

    Resized window, frame, and title

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 from tkinter import ttk
 
 TITLE = "Quantum CV"
-HEIGHT = 700
+HEIGHT = 400
 WIDTH = 700
 BROWSE_BUTTON_TEXT_1 = "Browse for image 1"
 BROWSE_BUTTON_TEXT_2 = "Browse for image 2"
@@ -15,19 +15,19 @@ root.title(TITLE)
 canvas = tk.Canvas(root, height = HEIGHT, width = WIDTH, bg = 'blue')
 canvas.pack()
 
-title = tk.Label(canvas, text = TITLE, font = ('Helvetica', 20), anchor = 'center')
-title.place(relx = .15, rely = .02, relwidth = .7, relheight = .05)
+title = tk.Label(canvas, text = TITLE, font = ('Helvetica', 18), anchor = 'center')
+title.place(relx = .25, rely = .0125, relwidth = .5, relheight = .075)
 
 frame_1 = tk.Frame(root)
-frame_1.place(relwidth=0.8, relheight=0.35, relx=0.5, rely=0.1, anchor='n')
+frame_1.place(relwidth=0.8, relheight=0.7, relx=0.5, rely=0.1, anchor='n')
 
-label_1 = tk.Label(frame_1, text = "Choose Image")
-label_1.place(relheight=0.1, relx=0.02)
+label_1 = tk.Label(frame_1, text = "Choose Image", font = ('Helvetica', 14))
+label_1.place(relheight=0.1, relx=0.02, rely = .03, anchor = "nw")
 
 image_entry_1 = tk.Entry(frame_1)
-image_entry_1.place(relwidth = .9, relheight = .2, relx = .05, rely = .4, anchor = 'nw')
+image_entry_1.place(relwidth = .9, relheight = .2, relx = .05, rely = .4, anchor = 'sw')
 image_entry_2 = tk.Entry(frame_1)
-image_entry_2.place(relwidth = .9, relheight = .2, relx = .05, rely = .4, anchor = 'sw')
+image_entry_2.place(relwidth = .9, relheight = .2, relx = .05, rely = .4, anchor = 'nw')
 
 browse_button_1 = tk.Button(frame_1, text = BROWSE_BUTTON_TEXT_1, command = lambda: image_file_selector(image_entry_1))
 browse_button_1.place(relx = .69, rely = .1, relwidth = .25, relheight = .13, anchor = 'e')
